8388089a235d91530595e82ba919e9c0b4219605
Matheus Penna <matheusnpenna@gmail.com>
2019-06-22 15:01:21 -0300

    Add draft-js-export-html

--- src/views/newPost/index.js
+++ src/views/newPost/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { EditorState, convertToRaw } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
-import { Form, Button, Container, Row } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import { stateToHTML } from 'draft-js-export-html';
 
 import { FireStore } from '../../config';
 
@@ -16,7 +17,8 @@ class NewPost extends React.Component {
         selectedBlogId: '',
         resume: '',
         editorState: EditorState.createEmpty(),
-        blogs: []
+        blogs: [],
+        postInHtml: ''
       };
 
       FireStore
@@ -31,22 +33,24 @@ class NewPost extends React.Component {
 
     onEditorStateChange = (editorState) => {
       const raw = editorState.getCurrentContent();
-      console.log(convertToRaw(raw));
+      const postInHtml = stateToHTML(raw); 
+      console.log(postInHtml);
       this.setState({
         editorState,
+        postInHtml
       });
     };
 
-    onTitleChange = (title) => {
-      this.setState({ title });
+    onTitleChange = (event) => {
+      this.setState({ title: event.target.value });
     }
 
-    onBlogChange = (selectedBlogId) => {
-      this.setState({ selectedBlogId });
+    onBlogChange = (event) => {
+      this.setState({ selectedBlogId: event.target.value });
     }
 
-    onResumeChange = (resume) => {
-      this.setState({ resume });
+    onResumeChange = (event) => {
+      this.setState({ resume: event.target.value });
     }
 
     onSubmit = (event) => {
@@ -54,13 +58,14 @@ class NewPost extends React.Component {
         title,
         resume,
         postBody,
-        selectedBlogId
+        selectedBlogId,
+        postInHtml
       } = this.state;
       FireStore
       .collection('blogs')
       .doc(selectedBlogId)
       .collection('posts')
-      .add({ title, resume, postBody });
+      .add({ title, resume, htmlBody: postInHtml });
     }
 
     render() {
@@ -68,7 +73,8 @@ class NewPost extends React.Component {
         title,
         resume,
         blogs, 
-        editorState
+        editorState,
+        postInHtml
       } = this.state;
 
       const renderBlogOptions = blogs.map(
@@ -77,40 +83,48 @@ class NewPost extends React.Component {
 
       return (
         <Container>
-          <Form onSubmit={this.onSubmit}>
-            <Form.Group>
-              <Form.Label>Blog</Form.Label>
-              <Form.Control as="select" onChange={this.onBlogChange}>
-                {renderBlogOptions}
-              </Form.Control>
-            </Form.Group>
+          <Row>
+            <Col sm={8}>
+              <Form onSubmit={this.onSubmit}>
+                <Form.Group>
+                  <Form.Label>Blog</Form.Label>
+                  <Form.Control as="select" onChange={this.onBlogChange}>
+                    {renderBlogOptions}
+                  </Form.Control>
+                </Form.Group>
 
-            <Form.Group>
-              <Form.Label>Título do Artigo</Form.Label>
-              <Form.Control type="text" value={title} onChange={this.onTitleChange} />
-            </Form.Group>
+                <Form.Group>
+                  <Form.Label>Título do Artigo</Form.Label>
+                  <Form.Control type="text" value={title} onChange={this.onTitleChange} />
+                </Form.Group>
 
-            <Form.Group>
-              <Form.Label>Resumo do artigo</Form.Label>
-              <Form.Control as="textarea" value={resume} onChange={this.onResumeChange} />
-            </Form.Group>
+                <Form.Group>
+                  <Form.Label>Resumo do artigo</Form.Label>
+                  <Form.Control as="textarea" value={resume} onChange={this.onResumeChange} />
+                </Form.Group>
 
-            <Form.Group>
-              <Form.Label>Conteúdo</Form.Label> 
-              <Editor
-                editorState={editorState}
-                onEditorStateChange={this.onEditorStateChange}
-                placeholder={'Escreve o conteúdo do artigo aqui'}
-                wrapperClassName="demo-wrapper"
-                className="editor"
-                editorClassName="demo-editor"
-                />
-            </Form.Group>
+                <Form.Group>
+                  <Form.Label>Conteúdo</Form.Label> 
+                  <Editor
+                    editorState={editorState}
+                    onEditorStateChange={this.onEditorStateChange}
+                    placeholder={'Escreve o conteúdo do artigo aqui'}
+                    wrapperClassName="demo-wrapper"
+                    className="editor"
+                    editorClassName="demo-editor"
+                    />
+                </Form.Group>
 
-            <Button variant="primary" type="submit">
-              Enviar
-            </Button>
-          </Form>
+                <Button variant="primary" type="submit">
+                  Enviar
+                </Button>
+              </Form>
+            </Col>
+            <Col sm={4}>
+              <iframe srcDoc={postInHtml} width={'100%'} height={'100%'}>
+              </iframe>
+            </Col>
+          </Row>
         </Container> 
       );
     }
